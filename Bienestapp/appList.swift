343eac93322c863ca5fe3c5ac782d19ff28d61a4
Fabregas <javier_fabregas_apps1ma1819@cev.com>
2020-01-28 13:32:27 +0100

    get data from api in feed

--- Bienestapp/appList.swift
+++ Bienestapp/appList.swift
@@ -6,21 +6,20 @@ class appList: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        getUsages()
+        
     }
     
     func getUsages(){
-             let url = ("http://localhost:8888/APIBienestapp/public/index.php/api/register")
-        
-        Alamofire.request(url).responseJSON {(response) in
-            if let json = response.result.value {
-                let jsonParseado = json as! [[String: Any]]
-                var usos: [Usos] = []
-                for uso in jsonParseado {
-                    usos.append(Usos(json: uso))
-                }
-                for uso in usos {
-                    print(uso.quote)
-                }
+        let url = URL(string: "http://localhost:8888/APIBienestapp/public/index.php/api/mostrar")
+        print("sesion iniciada")
+        let header = ["Authentication": "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJlbWFpbCI6Imphdmllcl9mYWJyZWdhc19hcHBzMW1hMTgxOUBjZXYuY29tIn0.CPmIcFFSltjwwdByDJAIS_EM4iuIeYZjKnkdv7KQM3E"]
+        Alamofire.request(url!, method: .get, parameters: nil, encoding: JSONEncoding.default, headers: header).responseJSON { (response) in
+            
+            let usages = response.result.value as! [[String:Any]]
+            
+            for use in usages{
+                print(use["application_id"]!)
             }
         }
     }

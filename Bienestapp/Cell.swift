bac3e6ab3229a5de9457557cc396abdda0426bd7
Fabregas <javier_fabregas_apps1ma1819@cev.com>
2020-01-29 13:22:27 +0100

    show apps and show usage finish

---
+++ Bienestapp/Cell.swift
@@ -0,0 +1,10 @@
+
+import UIKit
+
+class Cell: UITableViewCell {
+    
+    @IBOutlet weak var Imagen: UIImageView!
+    @IBOutlet weak var Nombre: UILabel!
+    @IBOutlet weak var TimeUse: UILabel!
+}
+
--- Bienestapp/appList.swift
+++ Bienestapp/appList.swift
@@ -1,26 +1,71 @@
 
 import UIKit
 import Alamofire
-class appList: UIViewController {
+import AlamofireImage
+class appList: UITableViewController {
+    var applications: [[String:Any]]?
+    var usages: [[String:Any]]?
+    var numberOfRows = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        getApplications()
         getUsages()
+        
+        tableView.delegate = self
+        tableView.dataSource = self
         
     }
     
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return numberOfRows
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "appCell", for: indexPath) as! Cell
+        if(self.applications != nil && self.usages != nil){
+            let url = URL(string: self.applications![indexPath.row]["icon"] as! String)
+            cell.Imagen.af_setImage(withURL: url!)
+            
+            cell.Nombre.text = (self.applications![indexPath.row]["name"] as! String)
+            cell.TimeUse.text = (self.usages![indexPath.row]["totalTime"] as! String)
+        }
+        return cell
+    }
+    func dataRecived(){
+        if(self.applications == nil && self.usages == nil){
+            return
+        }
+        
+        print(usages)
+        numberOfRows = applications!.count
+        self.tableView.reloadData()
+    }
     func getUsages(){
         let url = URL(string: "http://localhost:8888/APIBienestapp/public/index.php/api/mostrar")
         print("sesion iniciada")
         let header = ["Authentication": "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJlbWFpbCI6Imphdmllcl9mYWJyZWdhc19hcHBzMW1hMTgxOUBjZXYuY29tIn0.CPmIcFFSltjwwdByDJAIS_EM4iuIeYZjKnkdv7KQM3E"]
         Alamofire.request(url!, method: .get, parameters: nil, encoding: JSONEncoding.default, headers: header).responseJSON { (response) in
             
-            let usages = response.result.value as! [[String:Any]]
+            self.usages = response.result.value as? [[String:Any]]
+            self.dataRecived()
+        }
+        
+    }
+    
+    func getApplications () {
+        
+        let url = URL(string: "http://localhost:8888/APIBienestapp/public/index.php/api/application/var")
+        print("sesion iniciada")
+        let header = ["Authentication": "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJlbWFpbCI6Imphdmllcl9mYWJyZWdhc19hcHBzMW1hMTgxOUBjZXYuY29tIn0.CPmIcFFSltjwwdByDJAIS_EM4iuIeYZjKnkdv7KQM3E"]
+        
+        Alamofire.request(url!, method: .get, parameters: nil, encoding: JSONEncoding.default, headers: header).responseJSON { (response) in
             
-            for use in usages{
-                print(use["application_id"]!)
-            }
+            self.applications = response.result.value as! [[String:Any]]
+            self.dataRecived()
+            
         }
     }
 }
